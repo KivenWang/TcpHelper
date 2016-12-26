94ad1b2e8fe37c63f26cda9f69735b107041e159
Supegg <supegg.rao@gmail.com>
2016-12-26 14:18:55 +0800

    2016-12-26

---
+++ Client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using TcpHelper;
+
+namespace Client
+{
+    struct FileInfo
+    {
+        public string Name;
+        public string FullPath;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClientHelper client = new TcpClientHelper("127.0.0.1", 3699);
+            client.Start();
+            System.Threading.Tasks.Task task = new System.Threading.Tasks.Task(FileBackup, client);
+            task.Start();
+            Console.Read();
+        }
+
+        static void FileBackup(object arg)
+        {
+            TcpClientHelper client = (TcpClientHelper)arg;
+
+            //获取需备份的文件
+            //DataTable dt = this._oFileWatch.GetBackupFiles();
+            List<FileInfo> files = new List<FileInfo>();
+            files.Add(new FileInfo() { Name = "log.txt", FullPath = @"C:\log.txt" });
+            files.Add(new FileInfo() { Name = "dotNetFx40_Full_x86_x64.rar", FullPath = @"C:\dotNetFx40_Full_x86_x64.rar" });
+
+            foreach (var file in files)
+            {
+                client.SendMessage("FileBak");
+                //Thread.Sleep(10000);
+
+                if (client.ReadMessage().ToLower() == "filebakok")
+                {
+                    client.SendMessage(file.Name);
+                    client.SendFile(file.FullPath);
+                    client.SendMessage(client.CalcFileHash(file.FullPath));
+
+                    if (client.ReadMessage().ToLower() == "ok")
+                    {
+                        Console.WriteLine("备份文件【" + file.FullPath + "】成功");
+                    }
+                    else
+                    {
+                        Console.WriteLine("备份文件【" + file.FullPath + "】失败。");
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TcpHelper;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TcpListenerHelper listener = new TcpListenerHelper("127.0.0.1", 3699);
+            TcpListenerHelper listener = new TcpListenerHelper("0.0.0.0", 3699);
+            //TcpListenerHelper listener = new TcpListenerHelper("192.168.100.200", 3688);
+            listener.Start();
+            Task task = new Task(DoWork, listener);
+            task.Start();
+            Console.Read();
+
+        }
+
+        static void DoWork(object state)
+        {
+            TcpListenerHelper tlistener = (TcpListenerHelper)state;
+            tlistener.Listen();//监听
+            
+            while (tlistener.WaitForConnect())//等待直到监听到了连接
+            {
+                try
+                {
+                    string firstMessage = "";
+                    while (!string.IsNullOrEmpty((firstMessage = tlistener.ReadMessage())))
+                    {
+                        Console.WriteLine(firstMessage);
+                        if (firstMessage.ToLower() == "filebak".ToLower())
+                        {
+                            tlistener.SendMessage("filebakok");
+                            #region 文件备份
+                            string filepath = Path.Combine(Environment.CurrentDirectory, "FileBak\\" + tlistener.ReadMessage()).ToString();
+                            tlistener.ReceiveFile(filepath);
+                            if (tlistener.CalcFileHash(filepath) == tlistener.ReadMessage())
+                            {
+                                tlistener.SendMessage("ok");
+                                Console.WriteLine("接收到文件：{0}", filepath);
+                            }
+                            else
+                            {
+                                tlistener.SendMessage("wrong");
+                                Console.WriteLine("接收失败");
+                            }
+                            #endregion
+                        }
+                        else if (firstMessage.ToLower() == "DBBak".ToLower())
+                        {
+                            #region 数据库备份
+                            tlistener.SendMessage("dbbakok");
+                            string filename = tlistener.ReadMessage();
+                            string filepath = Path.Combine(System.Environment.CurrentDirectory, "DBBak") + "\\" + filename;
+                            //接收文件
+                            tlistener.ReceiveFile(filepath);
+                            //验证hash值
+                            string hash = tlistener.ReadMessage();
+                            if (hash == tlistener.CalcFileHash(filepath))
+                            {
+                                tlistener.SendMessage("ok");
+                                Console.WriteLine("接收到文件：{0}", filepath);
+                            }
+                            else
+                            {
+                                tlistener.SendMessage("wrong");
+                                Console.WriteLine("接收失败");
+                            }
+                            #endregion
+                        }
+                    }
+                }
+                catch
+                {
+                }
+
+                tlistener.Listen();//监听下一个连接
+            }
+        }
+    }
+}
